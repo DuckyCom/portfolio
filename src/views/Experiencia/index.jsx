58148a294c9518740b5da638a911c7654eac6f04
AMÉLIE TORRE WALSH <47757058@ort>
2024-05-22 15:07:36 -0300

    v3.2 experiencia listo, falta informacion en todo lo que le sigue

--- src/views/Experiencia/index.jsx
+++ src/views/Experiencia/index.jsx
@@ -1,5 +1,5 @@
 import Button from 'react-bootstrap/Button';
-
+import titulo from "../../components/Titulo";
 import imagenFullStack from '../../vendor/full-stack.svg';
 import imagenWEB from '../../vendor/web-programming.svg';
 import './style.css'
@@ -7,59 +7,66 @@ import cardIndex from '../../components/Card';
 
 import { Card, Row, Col, Container } from 'react-bootstrap';
 
-
+import ola from '../../vendor/wave4.svg';
 
 
 const Experiencia = () => {
     return (
-        <div className="experiencia">
-            <Container>
-                <Row>
-                    <Col md={6}>
-                        <Card className="mb-4">
-                            <div className="card-header d-flex justify-content-between">
-                                <div>
-                                    <h3 className="h5 mb-1">Desarrollador Full Stack</h3>
-                                    <div className="text-muted text-small">ORT Argentina 2024 - actualidad</div>
+        <>
+            <div id="experiencia">
+                <div className="titulo">
+                    <h1>Experiencia</h1>
+                </div>
+                <Container>
+                    
+                    <Row>
+                        <Col md={6}>
+                            <Card className="mb-4">
+                                <div className="card-header d-flex justify-content-between">
+                                    <div>
+                                        <h3 className="h5 mb-1">Desarrollador Full Stack</h3>
+                                        <div className="text-muted text-small">ORT Argentina 2024 - actualidad</div>
+                                    </div>
+                                    <img src={imagenFullStack} alt="Desarrollo WEB" className="img-fluid standard-image-size" />
                                 </div>
-                                <img src={imagenFullStack} alt="Desarrollo WEB" className="img-fluid standard-image-size" />
-                            </div>
-                            <div className="card-body">
-                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus cursus urna, ut finibus mi rutrum a. Cras placerat ornare dictum. Nulla facilisi. Aenean varius vehicula lacus nec elementum. Morbi scelerisque tellus id ex elementum, id congue neque porta. Maecenas aliquet sagittis nibh, ut sagittis est commodo eget. Cras purus quam, posuere et cursus nec, cursus vitae nibh. Vivamus a porta turpis, id porta ante. Proin feugiat ex ac risus maximus, quis euismod risus porttitor. Quisque eget varius turpis. Aliquam egestas leo quis justo ultrices, sit amet aliquam libero tincidunt. Morbi congue lorem at enim dictum maximus. Vestibulum at sodales nisi, sit amet rutrum urna.</Card.Text>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={6}>
-                        <Card className="mb-4">
-                            <div className="card-header d-flex justify-content-between">
-                                <div>
-                                    <h3 className="h5 mb-1">Desarrollador WEB1</h3>
-                                    <div className="text-muted text-small">ORT Argentina 2024 - actualidad</div>
+                                <div className="card-body">
+                                    <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus cursus urna, ut finibus mi rutrum a. Cras placerat ornare dictum. Nulla facilisi. Aenean varius vehicula lacus nec elementum. Morbi scelerisque tellus id ex elementum, id congue neque porta. Maecenas aliquet sagittis nibh, ut sagittis est commodo eget. Cras purus quam, posuere et cursus nec, cursus vitae nibh. Vivamus a porta turpis, id porta ante. Proin feugiat ex ac risus maximus, quis euismod risus porttitor. Quisque eget varius turpis. Aliquam egestas leo quis justo ultrices, sit amet aliquam libero tincidunt. Morbi congue lorem at enim dictum maximus. Vestibulum at sodales nisi, sit amet rutrum urna.</Card.Text>
                                 </div>
-                                <img src={imagenWEB} alt="Desarrollo WEB" className="img-fluid standard-image-size" />
-                            </div>
-                            <div className="card-body">
-                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus cursus urna, ut finibus mi rutrum a. Cras placerat ornare dictum. Nulla facilisi. Aenean varius vehicula lacus nec elementum. Morbi scelerisque tellus id ex elementum, id congue neque porta. Maecenas aliquet sagittis nibh, ut sagittis est commodo eget. Cras purus quam, posuere et cursus nec, cursus vitae nibh. Vivamus a porta turpis, id porta ante. Proin feugiat ex ac risus maximus, quis euismod risus porttitor. Quisque eget varius turpis. Aliquam egestas leo quis justo ultrices, sit amet aliquam libero tincidunt. Morbi congue lorem at enim dictum maximus. Vestibulum at sodales nisi, sit amet rutrum urna.</Card.Text>
-                            </div>
-                        </Card>
-                    </Col>
-                    <Col md={6}>
-                        <Card className="mb-4">
-                            <div className="card-header d-flex justify-content-between">
-                                <div>
-                                    <h3 className="h5 mb-1">Desarrollador WEB2</h3>
-                                    <div className="text-muted text-small">ORT Argentina 2024 - actualidad</div>
+                            </Card>
+                        </Col>
+                        <Col md={6}>
+                            <Card className="mb-4">
+                                <div className="card-header d-flex justify-content-between">
+                                    <div>
+                                        <h3 className="h5 mb-1">Desarrollador WEB1</h3>
+                                        <div className="text-muted text-small">ORT Argentina 2024 - actualidad</div>
+                                    </div>
+                                    <img src={imagenWEB} alt="Desarrollo WEB" className="img-fluid standard-image-size" />
                                 </div>
-                                <img src={imagenWEB} alt="Desarrollo WEB" className="img-fluid standard-image-size" />
-                            </div>
-                            <div className="card-body">
-                                <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus cursus urna, ut finibus mi rutrum a. Cras placerat ornare dictum. Nulla facilisi. Aenean varius vehicula lacus nec elementum. Morbi scelerisque tellus id ex elementum, id congue neque porta. Maecenas aliquet sagittis nibh, ut sagittis est commodo eget. Cras purus quam, posuere et cursus nec, cursus vitae nibh. Vivamus a porta turpis, id porta ante. Proin feugiat ex ac risus maximus, quis euismod risus porttitor. Quisque eget varius turpis. Aliquam egestas leo quis justo ultrices, sit amet aliquam libero tincidunt. Morbi congue lorem at enim dictum maximus. Vestibulum at sodales nisi, sit amet rutrum urna.</Card.Text>
-                            </div>
-                        </Card>
-                    </Col>
-                </Row>
-            </Container>
+                                <div className="card-body">
+                                    <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus cursus urna, ut finibus mi rutrum a. Cras placerat ornare dictum. Nulla facilisi. Aenean varius vehicula lacus nec elementum. Morbi scelerisque tellus id ex elementum, id congue neque porta. Maecenas aliquet sagittis nibh, ut sagittis est commodo eget. Cras purus quam, posuere et cursus nec, cursus vitae nibh. Vivamus a porta turpis, id porta ante. Proin feugiat ex ac risus maximus, quis euismod risus porttitor. Quisque eget varius turpis. Aliquam egestas leo quis justo ultrices, sit amet aliquam libero tincidunt. Morbi congue lorem at enim dictum maximus. Vestibulum at sodales nisi, sit amet rutrum urna.</Card.Text>
+                                </div>
+                            </Card>
+                        </Col>
+                        <Col md={6}>
+                            <Card className="mb-4">
+                                <div className="card-header d-flex justify-content-between">
+                                    <div>
+                                        <h3 className="h5 mb-1">Desarrollador WEB2</h3>
+                                        <div className="text-muted text-small">ORT Argentina 2024 - actualidad</div>
+                                    </div>
+                                    <img src={imagenWEB} alt="Desarrollo WEB" className="img-fluid standard-image-size" />
+                                </div>
+                                <div className="card-body">
+                                    <Card.Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus cursus urna, ut finibus mi rutrum a. Cras placerat ornare dictum. Nulla facilisi. Aenean varius vehicula lacus nec elementum. Morbi scelerisque tellus id ex elementum, id congue neque porta. Maecenas aliquet sagittis nibh, ut sagittis est commodo eget. Cras purus quam, posuere et cursus nec, cursus vitae nibh. Vivamus a porta turpis, id porta ante. Proin feugiat ex ac risus maximus, quis euismod risus porttitor. Quisque eget varius turpis. Aliquam egestas leo quis justo ultrices, sit amet aliquam libero tincidunt. Morbi congue lorem at enim dictum maximus. Vestibulum at sodales nisi, sit amet rutrum urna.</Card.Text>
+                                </div>
+                            </Card>
+                        </Col>
+                    </Row>
+                </Container>
         </div>
+        <img id="ola4" src={ola} alt="ola4"/>
+        </>
     )
 };
 
